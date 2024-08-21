d2100dd6a257501e44da2bda36114ace3f9cc51f
g3rfo <rozhchenkoivan@gmail.com>
2024-08-21 17:29:35 +0300

    Lab2.2

---
+++ Lab2/Lab2.2/function.py
@@ -0,0 +1,5 @@
+def function (n):
+    result = 0
+    for i in range(1, n + 1):
+        result += (i + 1)/i
+    return result
---
+++ Lab2/Lab2.2/main.py
@@ -0,0 +1,4 @@
+from function import function
+
+n = int(input("Enter n: "))
+print("Result of function: ", function(n))

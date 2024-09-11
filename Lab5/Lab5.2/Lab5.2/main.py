e01800895980146eac85c095516f52415ddfc2e4
g3rfo <rozhchenkoivan@gmail.com>
2024-09-11 19:04:12 +0300

    Lab5.2

--- Lab5/Lab5.2/Lab5.2/main.py
+++
@@ -1,4 +0,0 @@
-matrixSize = 7
-matrix = [[(matrixSize)*(i == j) + (matrixSize - j + i)*(i < j) + (matrixSize + j - i)*(i > j) for i in range(matrixSize)] for j in range(matrixSize)]
-for row in matrix:
-    print(*row)

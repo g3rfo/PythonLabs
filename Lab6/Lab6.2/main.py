403bf8ae44a046320f7dae5a72d3bacd7685ea8f
g3rfo <rozhchenkoivan@gmail.com>
2024-09-20 20:26:32 +0300

    Lab6.2

--- Lab6/Lab6.2/main.py
+++ Lab6/Lab6.2/main.py
@@ -1,7 +1,7 @@
-def every_second_element_function ():
+def every_second_element ():
     first_list = list(map(int , input('Enter a list: ').split()))
     print("Your list:", first_list)
     second_list = first_list[1::2]
     print("Second list(every second element of the first):", second_list)
 
-every_second_element_function()
+every_second_element()

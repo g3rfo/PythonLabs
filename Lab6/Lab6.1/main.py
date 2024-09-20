289e6838c0e51a0f47390316d3697ee4a317c9d5
g3rfo <rozhchenkoivan@gmail.com>
2024-09-20 17:47:51 +0300

    Lab6.1

---
+++ Lab6/Lab6.1/main.py
@@ -0,0 +1,8 @@
+def reverseAndLengthCalculationFunction ():
+    list = input('Enter a list: ').split()
+    print("Your list:", list)
+    list.reverse()
+    print("Reversed list:", list)
+    print("List length:", len(list))
+
+reverseAndLengthCalculationFunction()

4f3217dc338ac182d576b784ad0bd03b2fd721b7
g3rfo <rozhchenkoivan@gmail.com>
2024-09-20 18:03:46 +0300

    Lab6.2

---
+++ Lab6/Lab6.2/main.py
@@ -0,0 +1,7 @@
+def reverseAndLengthCalculationFunction ():
+    firstList = input('Enter a list: ').split()
+    print("Your list:", firstList)
+    secondList = firstList[1::2]
+    print("Second list(every second element of the first):", secondList)
+
+reverseAndLengthCalculationFunction()

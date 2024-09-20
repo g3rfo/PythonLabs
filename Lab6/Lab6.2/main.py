907c3eb8da14f06fc7ad0a6936ba1d428cc23505
g3rfo <rozhchenkoivan@gmail.com>
2024-09-20 20:53:36 +0300

    Lab6.2

--- Lab6/Lab6.2/main.py
+++ Lab6/Lab6.2/main.py
@@ -1,7 +1,10 @@
 def every_second_element ():
     first_list = list(map(int , input('Enter a list: ').split()))
     print("Your list:", first_list)
-    second_list = first_list[1::2]
-    print("Second list(every second element of the first):", second_list)
+    if len(first_list) > 1:
+        second_list = first_list[1::2]
+        print("Second list(every second element of the first):", second_list)
+    else:
+        print("Your list too small(< 2)")
 
 every_second_element()
